730ebbeed5c3886e9af3520de5fa44a92df6131b
libattistella <libattistella@gmail.com>
2019-09-25 14:03:24 -0300

    Static caching

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,39 @@
 self.addEventListener('install', (event) => {
   console.log('[Service Worker] Installing...', event);
+  event.waitUntil(caches.open('static').then((cache) => {
+    console.log('[Service Worker] Precaching app shell');
+    // cache.add('/');
+    // cache.add('/index.html');
+    // cache.add('/src/js/app.js');
+    cache.addAll([
+      '/',
+      '/index.html',
+      '/src/js/app.js',
+      '/src/js/feed.js',
+      '/src/js/promise.js', // Not necessary because older browsers neither support sw
+      '/src/js/fetch.js', // Not necessary because older browsers neither support sw
+      '/src/js/material.min.js',
+      '/src/css/app.css',
+      '/src/css/feed.css',
+      '/src/images/main-image.jpg',
+      'https://fonts.googleapis.com/css?family=Roboto:400,700',
+      'https://fonts.googleapis.com/icon?family=Material+Icons',
+      'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+    ])
+  })); // Esperamos que esta operación termine antes de seguir
 });
 
 self.addEventListener('activate', (event) => {
   console.log('[Service Worker] Activating...', event);
-  return self.clients.claim(); //Not necessary. Only makes it more robust
+  return self.clients.claim(); // Not necessary. Only makes it more robust
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log('[Service Worker] Fetching...', event);
-  event.respondWith(fetch(event.request));
+  event.respondWith(caches.match(event.request).then((res) => {
+    if (res) {
+      return res;
+    } else {
+      return fetch(event.request);
+    }
+  }));
 });
