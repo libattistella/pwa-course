4dd028106a251ef0bbb04bc99165769b97544d79
LAPTOP-EAOP0B70\Lucas <libattistella@gmail.com>
2019-09-28 21:34:13 -0300

    Best strategy until this momment working

--- public/sw.js
+++ public/sw.js
@@ -1,28 +1,31 @@
 var CACHE_STATIC_NAME = 'static-v5';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+var STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/promise.js', // Not necessary because older browsers neither support sw
+  '/src/js/fetch.js', // Not necessary because older browsers neither support sw
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
 
 self.addEventListener('install', (event) => {
   console.log('[Service Worker] Installing...', event);
-  event.waitUntil(caches.open(CACHE_STATIC_NAME).then((cache) => {
-    console.log('[Service Worker] Precaching app shell');
-    // Put send the request and store data when the response is back
-    cache.addAll([
-      '/',
-      '/index.html',
-      '/offline.html',
-      '/src/js/app.js',
-      '/src/js/feed.js',
-      '/src/js/promise.js', // Not necessary because older browsers neither support sw
-      '/src/js/fetch.js', // Not necessary because older browsers neither support sw
-      '/src/js/material.min.js',
-      '/src/css/app.css',
-      '/src/css/feed.css',
-      '/src/images/main-image.jpg',
-      'https://fonts.googleapis.com/css?family=Roboto:400,700',
-      'https://fonts.googleapis.com/icon?family=Material+Icons',
-      'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-    ])
-  })); // Esperamos que esta operación termine antes de seguir
+  event.waitUntil(caches.open(CACHE_STATIC_NAME)
+    .then((cache) => {
+      console.log('[Service Worker] Precaching app shell');
+      // Put send the request and store data when the response is back
+      cache.addAll(STATIC_FILES);
+    })
+  ); // Esperamos que esta operación termine antes de seguir
 });
 
 // Sólo se activará cuando el usuario cierre todas las pestañas vinculadas a la app y la abra de nuevo
@@ -42,17 +45,54 @@ self.addEventListener('activate', (event) => {
 
 // First cache, then network
 self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.open(CACHE_DYNAMIC_NAME)
-    .then((cache) => {
-      return fetch(event.request)
-        .then((res) => {
-          cache.put(event.request, res.clone());
+  var url = 'https://httpbin.org/get';
+
+  if (event.request.url.indexOf(url) > -1) {
+    // "Cache then network". Buscamos en la red, y guardamos en la cache
+    event.respondWith(caches.open(CACHE_DYNAMIC_NAME)
+      .then((cache) => {
+        return fetch(event.request)
+          .then((res) => {
+            cache.put(event.request, res.clone());
+            return res;
+          })
+      })
+    );
+  } else if (isInArray(event.request.url, STATIC_FILES)) {
+    // "Cache only" strategy for static files.
+    // Debemos asegurarnos de actualizar la versión de la caché si actualizamos algunos de estos archivos. Es opcional.
+    event.respondWith(caches.match(event.request));
+  } else {
+    // "Cache with network fallback" strategy
+    event.respondWith(caches.match(event.request)
+      .then((res) => {
+        if (res) {
           return res;
-        })
-    })
-  );
+        } else {
+          return fetch(event.request)
+            .then((res) => {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then((cache) => {
+                  // Put only store data, I must provide the request and the response
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                });
+            })
+            .catch((err) => {
+              return caches.open(CACHE_STATIC_NAME)
+                .then((cache) => {
+                  if (event.request.headers.get('accept').includes('text/html')) {
+                    return cache.match('/offline.html');
+                  }
+                })
+            });
+        }
+      })
+    );
+  }
 });
 
+// First cache, if not cache, then network
 // self.addEventListener('fetch', (event) => {
 //   event.respondWith(caches.match(event.request)
 //     .then((res) => {
@@ -77,7 +117,7 @@ self.addEventListener('fetch', (event) => {
 //   );
 // });
 
-// First network, then cache
+// First network, if it fails, then cache
 // self.addEventListener('fetch', (event) => {
 //   event.respondWith(fetch(event.request)
 //     .then((res) => {
@@ -102,3 +142,12 @@ self.addEventListener('fetch', (event) => {
 // self.addEventListener('fetch', (event) => {
 //   event.respondWith(fetch(event.request))
 // });
+
+function isInArray(url, arr) {
+  arr.forEach((elem) => {
+    if (elem === url) {
+      return true;
+    }
+  });
+  return false;
+}
