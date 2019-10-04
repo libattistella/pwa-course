a7c73bdb225d18e3683b8b1a34381b82155cb783
libattistella <libattistella@gmail.com>
2019-10-04 09:20:51 -0300

    Before firebase deploy

---
+++ functions/index.js
@@ -0,0 +1,26 @@
+/* eslint-disable promise/always-return */
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const cors = require('cors')({
+  origin: true
+});
+
+exports.storePostData = functions.https.onRequest((request, response) => {
+ cors((request, response) => {
+   admin.database().ref('posts').push({
+    id: request.body.id,
+    title: request.body.title,
+    location: request.body.location,
+    image: request.body.image
+   }).then(() => {
+    response.status(201).json({
+      message: 'Data stored!',
+      id: request.body.id
+     });
+   }).catch((err) => {
+    response.status(500).json({
+      error: err
+    });
+   });
+ });
+});
