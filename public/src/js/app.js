8e383f976edf8bdab102fd145c4683ff4b67b3f0
libattistella <libattistella@gmail.com>
2019-10-07 09:47:35 -0300

    Notifications options

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -21,6 +21,30 @@ window.addEventListener('beforeinstallprompt', (event) => {
   return false;
 });
 
+var displayConfirmNotification = () => {
+  if ('serviceWorker' in navigator) {
+    var options = {
+      body: 'You successfully subscribed to our Notification service',
+      icon: '/src/images/icons/app-icon-96x96.png',
+      image: '/src/images/sf-boat.jpg',
+      dir: 'ltr', // Default
+      lang: 'en-US',
+      vibrate: [100, 50, 200],
+      badge: '/src/images/icons/app-icon-96x96.png',
+      tag: 'confirm-notification',
+      renotify: true,
+      actions: [
+        { action: 'confirm', title: 'OK', icon: '/src/images/icons/app-icon-96x96.png' },
+        { action: 'cancel', title: 'CANCEL', icon: '/src/images/icons/app-icon-96x96.png' }
+      ]
+    };
+    navigator.serviceWorker.ready
+      .then((swReg) => {
+        swReg.showNotification('Successfully subscribed! (from SW)', options);
+      })
+  }
+}
+
 var askForPermission = () => {
   Notification.requestPermission((result) => {
     console.log('User choise', result);
@@ -28,6 +52,7 @@ var askForPermission = () => {
       console.log('Notification persmission denied');
     } else {
       // Hide buttons
+      displayConfirmNotification();
     }
   });
 }
--- public/sw.js
+++ public/sw.js
@@ -216,3 +216,31 @@ self.addEventListener('sync', (event) => {
     );
   }
 });
+
+self.addEventListener('notificationclick', (event) => {
+  var notification = event.notification;
+  var action = event.action;
+
+  console.log(notification);
+  if (action === 'confirm') {
+    console.log('Confirm was chosen');
+  } else {
+    console.log(action);
+  }
+  notification.close();
+});
+
+self.addEventListener('notificationclose', (event) => {
+  var notification = event.notification;
+  var action = event.action;
+
+  console.log('Confirm was closed', event);
+
+  console.log(notification);
+  if (action === 'confirm') {
+    console.log('Confirm was chosen');
+  } else {
+    console.log(action);
+  }
+  notification.close();
+});
