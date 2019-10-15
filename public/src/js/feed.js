a19c9ca21d47a605a2c05a2eaf73a6e4c83626aa
libattistella <libattistella@gmail.com>
2019-10-15 08:38:20 -0300

    Preparing project for native device features

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -5,11 +5,21 @@ var sharedMomentsArea = document.querySelector('#shared-moments');
 var form = document.querySelector('form');
 var titleInput = document.querySelector('#title');
 var locationInput = document.querySelector('#location');
+var videoPlayer = document.querySelector('#player');
+var canvasElement = document.querySelector('#canvas');
+var captureButton = document.querySelector('#capture-btn');
+var imagePicker = document.querySelector('#image-picker');
+var imagePickerArea = document.querySelector('#pick-image');
+
+var initializeMedia = () => {
+
+}
 
 function openCreatePostModal() {
   // createPostArea.style.display = 'block';
   // setTimeout(function() {
   createPostArea.style.transform = 'translateY(0)';
+  initializeMedia();
   // }, 1);
   if (deferredPrompt) {
     deferredPrompt.prompt();
